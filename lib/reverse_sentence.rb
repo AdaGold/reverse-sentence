3c9be6451dc01ebd70c075ad5382708431c5e3ea
stpatrickschild <hannahtekie@gmail.com>
2020-03-18 18:54:59 -0700

    worked on reverse and sort code together w/Haben, worked on  time/space complexity alone.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,51 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
+# Time complexity: ?  
+# n = mysentence length
+# m = newsentence length
+# n + 1/2m + m =  n + 3/2m => O(n + m) (1/2 n b/c since it's decrementing and increamenting at the same time)
+
 # Space complexity: ?
+# n = new_arr length == my_sentence
+# m = holder length stores only words without space,
+# p = new_sentence
+# n + m + p => O(n + m + p)
+
+
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return nil if my_sentence.nil?
+  new_arr = []
+  holder = []
+  i = 0
+  while i < my_sentence.length
+    if my_sentence[i] == " "
+      new_arr << holder.join # ["Hello"]
+      new_arr << " "   # ["Hello", " "]
+      holder = [] # empty array for the next word
+    else
+      holder << my_sentence[i]
+    end
+    i += 1
+  end
+  new_arr << holder.join # ["Hello", " ", "world"]
+  # creating the reversed string from the above array
+  k = new_arr.length - 1
+  i = 0
+  while k >= 0
+    if i < k
+      temp = new_arr[i] # temporarly save words in the first half of the array
+      new_arr[i] = new_arr[k] # swap each words in the array
+      new_arr[k] = temp
+      i += 1
+    end
+    k -= 1
+  end
+  # ["world", " ", "Hello"]
+  i = 0
+  new_sentence = new_arr.join
+  while i < new_sentence.length
+    my_sentence[i] = new_sentence[i] #my_sentence at index 0 is replaced by new_sentence at index 0
+    i += 1
+  end
+  return my_sentence
 end
+
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -1,7 +1,36 @@
 # A method which will return an array of the words in the string
 #  sorted by the length of the word.
 # Time complexity: ?
+# to why it was not => nlogn
+# i   j 
+# 1   1 
+# 2   2
+# 3   3
+# 4   4
+# 5   5
+# .   .
+# .   .
+# n   n
+
+# n + n(n+1)/2 = n +  (n2 + n)/2  => O(n2)
+
 # Space complexity: ?
+# n => array 
+# O(n)
+
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array = my_sentence.split(" ")
+  i = 1
+  while i < array.length()
+    temp = array[i]
+    # puts temp
+    j = i - 1
+    while j >= 0 && temp.length() < array[j].length()
+      array[j + 1] = array[j]
+      j -= 1
+    end
+    array[j + 1] = temp
+    i += 1
+  end
+  return array
 end
