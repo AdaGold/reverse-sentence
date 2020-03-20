8b834b7825de44b0f4799e39fb9a1937555cfd1b
Kate Mangubat <kate.d.mangubat@gmail.com>
2020-03-20 11:03:37 -0700

    reverse sentence and sort by length passing tests

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,50 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) each iteration is independent of each other
+# Space complexity: O(n) because of string needing to be split for first step in the method
+
+#example: coffee is the best = > best is the coffee
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
-end
+    #check if my_sentence even exists
+  if my_sentence.nil? || my_sentence == "" || my_sentence.length == 0
+    return my_sentence
+  end
+
+  #example input: "coffee is the best"
+  #output: #output: "best is the coffee"
+  #                             0      1      2       3
+  #store into temp array = ["coffee", "is", "the", "best"]
+  #split sentence
+  #
+  temp_array = my_sentence.split(/(\s+|\S+)/)
+
+  #make another array for storage = reversed_array = []
+  reversed_array = []
+  #loop, start at lastword = temp array.length - 1
+  i = temp_array.length - 1
+  while i >= 0
+    # push word = "best" into reversed_array
+    reversed_array.push(temp_array[i])
+    #decrement  
+    i -= 1
+  end
+  
+  # ['12','34','35','231'].join(', ')
+  reversed_sentence = reversed_array.join("")
+  
+  #clear the input using mutations http://rubyblog.pro/2017/09/pass-by-value-or-pass-by-reference
+  my_sentence.clear
+
+  #use string manipulation
+  my_sentence.concat(reversed_sentence)
+
+  #cannot do this because ruby is pass by value and not reference
+  #my_sentence = reversed_sentence
+end #end
+
+# test_string = "  I can do this!     "
+# "  I can do this!     "
+# puts test_string
+# reverse_sentence(test_string)
+# puts test_string
+
+
--- lib/sort_by_length.rb
+++ lib/sort_by_length.rb
@@ -3,5 +3,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def sort_by_length(my_sentence)
-  raise NotImplementedError, "Method not implemented"
+  array_of_words = []
+
+  if my_sentence == ""
+    return array_of_words
+  end
+
+  array_of_words = my_sentence.split
+
+  first_word = array_of_words[0]
+  (1..array_of_words.length-1).each do |i|
+    current_word = array_of_words[i]
+    if current_word.length < first_word.length
+      array_of_words[i] = first_word
+      array_of_words[i-1] = current_word
+    end
+    first_word = current_word
+  end
+  return array_of_words
 end
